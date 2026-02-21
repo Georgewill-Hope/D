10378cd2c37bd6e2995310ecaa49d36e8f19edd9
Georgewill Hope <georgeblue222@gmail.com>
2026-02-21 16:09:01 +0100

    Added SEO to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,28 @@ import Featured from "@/components/Featured";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 import bannerImage from "@/public/assets/saman-taheri-79FmeCtL9NE-unsplash.jpg"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | DD Perfumes",
+  description: "Best Selling Perfume in the world",
+  keywords: ["DD", "Perfumes", "Oil Perfumes", "DD Oil Perfumes"],
+  openGraph: {
+    title: "Home | DD Perfumes",
+    description: "Best Selling Perfume in the world",
+    siteName: "https://dd-perfumes-hopedev.vercel.app",
+    locale: "en_US",
+    images: ["https://dd-perfumes-hopedev.vercel.app/opengraph-image.png"],
+  },
+  twitter: {
+    title: "Home | DD Perfumes",
+    description: "Best Selling Perfume in the world",
+    card: "summary_large_image",
+    images: ["https://dd-perfumes-hopedev.vercel.app/opengraph-image.png"],
+    creator: "@Hopedev_",
+  },
+  authors: [{ name: "DD Prefumes" }],
+};
 
 export default function Home() {
   return (
